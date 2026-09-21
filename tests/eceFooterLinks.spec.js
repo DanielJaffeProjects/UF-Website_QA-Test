7d2f4f993d69a3013597838118a5fbd46d878523
Daniel Jaffe <drjeducation@outlook.com>
2026-09-21 16:05:58 -0400

    feat - added  automated social media clicks in footer

--- tests/eceFooterLinks.spec.js
+++ tests/eceFooterLinks.spec.js
@@ -1,9 +1,16 @@
 import {test, expect} from '@playwright/test';
 
+test.beforeEach(async ({ page }) => {
+    await page.goto('https://www.ece.ufl.edu/');
+});
+
+test.afterEach(async ({ page }) => {
+    await page.close();
+});
+
+
 //testing all the footer links on the uf ece website
 test('Footer Links', async ({ page }) => {
-    await page.goto('https://www.ece.ufl.edu/');
-
 
     const links = [
     "Directory",
@@ -31,5 +38,22 @@ test('Footer Links', async ({ page }) => {
     
     for (let i = 0; i < links.length; i++) {
         const link = links[i];
+        await page.getByRole('link', { name: link , exact: true }).click();    }
+});
+
+test('Footer Social Media Links', async ({ page }) => {
+
+    const socialLinks = [
+    "Facebook",
+    "X (formerly Twitter)",
+    "YouTube",
+    "LinkedIn",
+    "Instagram",
+    "News Feed"
+    ];
+    
+    for (let i = 0; i < socialLinks.length; i++) {
+        await page.goto('https://www.ece.ufl.edu/');
+        const link = socialLinks[i];
         await page.getByRole('link', { name: link , exact: true }).click();    }
 });
